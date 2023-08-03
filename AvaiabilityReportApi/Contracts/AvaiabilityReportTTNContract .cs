64a7f14f0b0ecb3f45eb60bdf7a1fe52e7888b03
Marcin Ruta <m.ruta@stud.elka.pw.edu.pl>
2023-08-03 19:34:25 +0200

    Added new endpoint used for TTN integration

---
+++ AvaiabilityReportApi/Contracts/AvaiabilityReportTTNContract .cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+namespace AvaiabilityReportApi.Contracts
+{
+    public class AvaiabilityReportTTNContract
+    {
+        public EndDeviceIds end_device_ids { get; set; }
+        public List<string> correlation_ids { get; set; }
+        public DateTime received_at { get; set; }
+        public UplinkMessage uplink_message { get; set; }
+    }
+
+    public class ApplicationIds
+    {
+        public string application_id { get; set; }
+    }
+
+    public class EndDeviceIds
+    {
+        public string device_id { get; set; }
+        public ApplicationIds application_ids { get; set; }
+        public string dev_eui { get; set; }
+        public string join_eui { get; set; }
+        public string dev_addr { get; set; }
+    }
+
+    public class GatewayIds
+    {
+        public string gateway_id { get; set; }
+        public string eui { get; set; }
+    }
+
+    public class RxMetadata
+    {
+        public GatewayIds gateway_ids { get; set; }
+        public long timestamp { get; set; }
+        public int rssi { get; set; }
+        public int channel_rssi { get; set; }
+        public double snr { get; set; }
+        public string uplink_token { get; set; }
+        public int channel_index { get; set; }
+        public DateTime received_at { get; set; }
+    }
+
+    public class DataRate
+    {
+        public LoRa lora { get; set; }
+    }
+
+    public class LoRa
+    {
+        public int bandwidth { get; set; }
+        public int spreading_factor { get; set; }
+        public string coding_rate { get; set; }
+    }
+
+    public class Settings
+    {
+        public DataRate data_rate { get; set; }
+        public string frequency { get; set; }
+        public long timestamp { get; set; }
+    }
+
+    public class DecodedPayload
+    {
+        public bool occupancy { get; set; }
+    }
+
+    public class UplinkMessage
+    {
+        public string session_key_id { get; set; }
+        public int f_port { get; set; }
+        public int f_cnt { get; set; }
+        public string frm_payload { get; set; }
+        public DecodedPayload decoded_payload { get; set; }
+        public List<RxMetadata> rx_metadata { get; set; }
+        public Settings settings { get; set; }
+        public DateTime received_at { get; set; }
+        public string consumed_airtime { get; set; }
+        public NetworkIds network_ids { get; set; }
+    }
+
+    public class NetworkIds
+    {
+        public string net_id { get; set; }
+        public string tenant_id { get; set; }
+        public string cluster_id { get; set; }
+        public string cluster_address { get; set; }
+    }
+}
--- AvaiabilityReportApi/Controllers/AvaiabilityReportController.cs
+++ AvaiabilityReportApi/Controllers/AvaiabilityReportController.cs
@@ -63,8 +63,36 @@ namespace AvaiabilityReportApi.Controllers
             }
         }
 
+        [Route("TTN")]
+        [HttpPost]
+        public async Task<ActionResult> PostTTNStatus([FromBody] AvaiabilityReportTTNContract avaiabilityReport)
+        {
+            try
+            {
+                var machine = await this.AvaiabilityReportRepository.GetMachine(avaiabilityReport.end_device_ids.dev_eui);
+                if (machine == null)
+                {
+                    return BadRequest("Device has not been added into the system");
+                }
+
+                var newAvaiabilityReport = new AvaiabilityReportDto { CurrentState = avaiabilityReport.uplink_message.decoded_payload.occupancy.ToString(), Machine = machine, Timestamp = DateTime.Now };
+                var newReportStatus = await this.AvaiabilityReportRepository.AddAvaiabilityReport(newAvaiabilityReport);
+
+                if (newReportStatus == null)
+                {
+                    return NoContent();
+                }
+
+                return Ok(newReportStatus);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+
     }
 
-   
-    
+
 }
